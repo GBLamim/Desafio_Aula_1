48156178d2806c035286e684b71d323cd221db7f
GBLamim <guilherme.lamim@projuris.com.br>
2022-08-17 19:17:43 -0300

    html

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 # coding: utf-8
-import re
-import json
-import urllib
+import re, json, urllib
+#import json
+#import urllib
 from flask import Flask, render_template, request
 app = Flask("challenge")
 
@@ -13,32 +13,7 @@ def none():
 def index():
     name = urllib.parse.unquote(re.sub(r'.*?login\_name\=(.*?)(\&.*|$)', '\g<1>', request.full_path.replace('+', ' ')))
     print(name)
-    variaveis = {
-        "motive":"Desafio da Primeira Aula",
-        "table":"Produtos",
-        "array":[
-            {
-                "name":"Garrafa",
-                "value":"12.99"
-            },
-            {
-                "name":"Calculadora",
-                "value":"4.98"
-            },
-            {
-                "name":"Impressora",
-                "value":"875.65"
-            },
-            {
-                "name":"Mouse",
-                "value":"75.95"
-            },
-            {
-                "name":"Vaso",
-                "value":"80.0"
-            }
-        ]
-    }
+    variaveis = {"motive":"Desafio da Primeira Aula","table":"Produtos","array":[{"name":"Garrafa","value":"12.99"},{"name":"Calculadora","value":"4.98"},{"name":"Impressora","value":"875.65"},{"name":"Mouse","value":"75.95"},{"name":"Vaso","value":"80.0"}]}
     variaveis = json.loads(re.sub(r'(\"value\"\s?\:\s?\"\d+\.\d)\"', '\g<1>0"', json.dumps(variaveis)))
     return render_template("index.html", n = name, v = variaveis), 200
 
